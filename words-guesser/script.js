5d66e56bf5a244500ca0fed53d0ae5dca01b6c2c
Niels van Schagen <s3649288@vuw.leidenuniv.nl>
2023-05-12 15:32:19 +0200

    pretty

--- words-guesser/script.js
+++ words-guesser/script.js
@@ -1,4 +1,38 @@
-fetch("words.json")
-    .then(response => response.json())
-    .then(data => console.log(data))
-    .catch(error => alert(`Error: ${error}`));
+class Game {
+    words = undefined;
+    constructor() {
+        fetch("words.json")
+            .then(response => response.json())
+            .then(data => words = data)
+            .catch(error => alert(`Error: ${error}`));
+    }
+}
+
+class Stage {
+    constructor(id) {
+        this.container = document.getElementById(id);
+    }
+
+    show() {
+        for (elem of document.getElementsByClassName("shown")) {
+            elem.classList.remove("shown");
+        }
+        this.container.classList.add("shown");
+    }
+}
+
+addEventListener("DOMContentLoaded", () => {
+    const stages = {
+        index: new Stage("index"),
+        prepare: new Stage("prepare"),
+        active: new Stage("active")
+    }
+    
+    const game = new Game();
+
+    for (elem of document.getElementsByClassName("redirector")) {
+        elem.addEventListener("click", event => {
+            stages[event.target.dataset.redirect].show();
+        });
+    }
+});
